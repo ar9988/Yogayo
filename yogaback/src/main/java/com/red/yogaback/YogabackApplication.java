17a01a7efbd40f26a9d96157dff4087f59ad57c8
경이현 <wm0755123@naver.com>
2025-03-13 15:30:32 +0900

    S12P21D104-50 feat: 스프링 프로젝트 생성

---
+++ yogaback/src/main/java/com/red/yogaback/YogabackApplication.java
@@ -0,0 +1,13 @@
+package com.red.yogaback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YogabackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(YogabackApplication.class, args);
+    }
+
+}
