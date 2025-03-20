704323385e9647ce585e0bd8135502211076d440
경이현 <wm0755123@naver.com>
2025-03-20 15:49:19 +0900

    S12P21D104-49 fix: DB 재생성

--- yogaback/src/test/java/com/red/yogaback/YogabackApplicationTests.java
+++ yogaback/src/test/java/com/red/yogaback/YogabackApplicationTests.java
@@ -1,9 +1,8 @@
 package com.red.yogaback;
 
+
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.ActiveProfiles;
-import org.springframework.test.context.TestPropertySource;
 
 
 @SpringBootTest(classes = YogabackApplicationTests.class)
@@ -12,6 +11,7 @@ class YogabackApplicationTests {
 
     @Test
     void contextLoads(){
+
 
     }
 
