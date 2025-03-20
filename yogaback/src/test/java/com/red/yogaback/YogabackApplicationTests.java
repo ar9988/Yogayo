83617947c7cc98b42203d505221ac3e43b3c4709
경이현 <wm0755123@naver.com>
2025-03-20 14:36:57 +0900

    S12P21D104-40 fix: 프로필 주석 처리

--- yogaback/src/test/java/com/red/yogaback/YogabackApplicationTests.java
+++ yogaback/src/test/java/com/red/yogaback/YogabackApplicationTests.java
@@ -7,7 +7,7 @@ import org.springframework.test.context.TestPropertySource;
 
 
 @SpringBootTest(classes = YogabackApplicationTests.class)
-@ActiveProfiles("test")
+//@ActiveProfiles("test")
 class YogabackApplicationTests {
 
     @Test
