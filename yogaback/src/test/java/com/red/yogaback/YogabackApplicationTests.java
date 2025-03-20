170e013327caee659ba01307d31ccee9da06d5b8
경이현 <wm0755123@naver.com>
2025-03-20 16:00:17 +0900

    S12P21D104-40 test: 테스트 설정

--- yogaback/src/test/java/com/red/yogaback/YogabackApplicationTests.java
+++ yogaback/src/test/java/com/red/yogaback/YogabackApplicationTests.java
@@ -2,16 +2,21 @@ package com.red.yogaback;
 
 
 import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.test.context.SpringBootTest;
+
 
 
 @SpringBootTest(classes = YogabackApplicationTests.class)
 //@ActiveProfiles("test")
 class YogabackApplicationTests {
 
+    private static final Logger log = LoggerFactory.getLogger(YogabackApplicationTests.class);
+
     @Test
     void contextLoads(){
-
+        log.info("테스트 코드 시작");
 
     }
 
