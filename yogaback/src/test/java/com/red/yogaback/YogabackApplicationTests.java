79089dec18e6879066fadad17ec8ccdfd39dce6f
경이현 <wm0755123@naver.com>
2025-03-20 14:20:30 +0900

    S12P21D104-40 feat: 테스트 디렉토리에 yml 추가

--- yogaback/src/test/java/com/red/yogaback/YogabackApplicationTests.java
+++ yogaback/src/test/java/com/red/yogaback/YogabackApplicationTests.java
@@ -3,8 +3,9 @@ package com.red.yogaback;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.TestPropertySource;
 
-@ActiveProfiles("test")
+@TestPropertySource("classpath:application-test.yml")
 @SpringBootTest
 class YogabackApplicationTests {
 
