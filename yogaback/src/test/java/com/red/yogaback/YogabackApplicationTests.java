110c96bddf0abe819c009f572d557507d0444eb6
경이현 <wm0755123@naver.com>
2025-03-20 14:28:09 +0900

    S12P21D104-40 feat: 테스트 프로필 추가

--- yogaback/src/test/java/com/red/yogaback/YogabackApplicationTests.java
+++ yogaback/src/test/java/com/red/yogaback/YogabackApplicationTests.java
@@ -7,6 +7,7 @@ import org.springframework.test.context.TestPropertySource;
 
 
 @SpringBootTest(classes = YogabackApplicationTests.class)
+@ActiveProfiles("test")
 class YogabackApplicationTests {
 
     @Test
