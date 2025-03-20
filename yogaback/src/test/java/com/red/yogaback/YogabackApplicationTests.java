f7e9a971b18bc043aa5ce320b07057d0ae2882b5
경이현 <wm0755123@naver.com>
2025-03-20 12:39:26 +0900

    S12P21D104-40 fix: 젠킨스에 테스트 적용

--- yogaback/src/test/java/com/red/yogaback/YogabackApplicationTests.java
+++ yogaback/src/test/java/com/red/yogaback/YogabackApplicationTests.java
@@ -2,12 +2,15 @@ package com.red.yogaback;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
+@ActiveProfiles(profiles = "prod")
 @SpringBootTest
 class YogabackApplicationTests {
 
-////    @Test
-////    void contextLoads() {
-////    }
+    @Test
+    void contextLoads(){
+
+    }
 
 }
