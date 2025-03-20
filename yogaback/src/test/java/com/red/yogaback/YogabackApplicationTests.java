42c5696456da4cb7671d146a6ad6d0adea9a6f92
경이현 <wm0755123@naver.com>
2025-03-20 16:40:42 +0900

    S12P21D104-40 feat: gradle 빌드 시 프로필 설정

--- yogaback/src/test/java/com/red/yogaback/YogabackApplicationTests.java
+++ yogaback/src/test/java/com/red/yogaback/YogabackApplicationTests.java
@@ -5,11 +5,11 @@ import org.junit.jupiter.api.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.test.context.SpringBootTest;
-
+import org.springframework.test.context.ActiveProfiles;
 
 
 @SpringBootTest
-//@ActiveProfiles("test")
+@ActiveProfiles("prod")
 class YogabackApplicationTests {
 
     private static final Logger log = LoggerFactory.getLogger(YogabackApplicationTests.class);
