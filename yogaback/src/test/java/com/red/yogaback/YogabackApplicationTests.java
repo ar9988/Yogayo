5357f0cc262c29b942022d8ea248e27d72c065f6
경이현 <wm0755123@naver.com>
2025-03-21 10:22:32 +0900

    S12P21D104-40 feat: 젠킨스 테스트 설정

--- yogaback/src/test/java/com/red/yogaback/YogabackApplicationTests.java
+++ yogaback/src/test/java/com/red/yogaback/YogabackApplicationTests.java
@@ -6,10 +6,11 @@ import org.junit.jupiter.api.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 
-
-@SpringBootTest(classes = YogabackApplicationTests.class)
+@SpringBootTest
+@ActiveProfiles("prod")
 class YogabackApplicationTests {
 
     private static final Logger log = LoggerFactory.getLogger(YogabackApplicationTests.class);
