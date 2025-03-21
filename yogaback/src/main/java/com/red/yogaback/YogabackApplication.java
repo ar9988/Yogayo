e4afcef610b07024f6a4d861745f83b039c2ba62
경이현 <wm0755123@naver.com>
2025-03-21 16:09:45 +0900

    S12P21D104-41 feat: nginx 서버 url 추가

--- yogaback/src/main/java/com/red/yogaback/YogabackApplication.java
+++ yogaback/src/main/java/com/red/yogaback/YogabackApplication.java
@@ -1,10 +1,13 @@
 package com.red.yogaback;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.servers.Server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 
 @SpringBootApplication
+@OpenAPIDefinition(servers = {@Server(url = "https://j12d104.p.ssafy.io",description = "https"), @Server(url = "http://localhost:8080",description = "로컬")})
 public class YogabackApplication {
 
     public static void main(String[] args) {
