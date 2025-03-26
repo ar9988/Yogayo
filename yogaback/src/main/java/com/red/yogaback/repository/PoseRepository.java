901d4ed1c3d6a7c8d35778aafa343886abb45f9e
sm <ssafysm@gmail.com>
2025-03-26 14:00:20 +0900

    S12P21D104-73 feat: fix code

--- yogaback/src/main/java/com/red/yogaback/repository/PoseRepository.java
+++ yogaback/src/main/java/com/red/yogaback/repository/PoseRepository.java
@@ -4,11 +4,8 @@ import com.red.yogaback.model.Pose;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
-
 @Repository
 public interface PoseRepository extends JpaRepository<Pose, Long> {
     // 기본 CRUD 메서드는 JpaRepository에서 제공됨
     // 필요한 경우 추가 쿼리 메서드를 여기에 정의할 수 있음
-    List<Pose> findAll();
 }
