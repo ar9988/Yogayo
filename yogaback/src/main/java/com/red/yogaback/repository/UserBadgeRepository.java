d0a650c40aedb522a91aeef570604028bb6ff530
경이현 <wm0755123@naver.com>
2025-03-26 17:27:17 +0900

    S12P21D104-68 fix: 배지 체크 리팩토링

--- yogaback/src/main/java/com/red/yogaback/repository/UserBadgeRepository.java
+++ yogaback/src/main/java/com/red/yogaback/repository/UserBadgeRepository.java
@@ -4,6 +4,8 @@ import com.red.yogaback.model.Badge;
 import com.red.yogaback.model.User;
 import com.red.yogaback.model.UserBadge;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -14,6 +16,10 @@ public interface UserBadgeRepository extends JpaRepository<UserBadge, Long> {
     List<UserBadge> findByUser(User user);
 
     UserBadge findByUserAndBadge(User user, Badge badge);
+
+    @Query("SELECT ub FROM UserBadge ub JOIN FETCH ub.badge b LEFT JOIN FETCH b.badgeDetails " +
+            "WHERE ub.user = :user AND ub.isNew = true")
+    List<UserBadge> findNewBadgesWithDetails(@Param("user") User user);
 
 
 }
