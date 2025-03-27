c7b4a705939a077658cabe59cb21d3e8593fbe1b
sm <ssafysm@gmail.com>
2025-03-27 17:59:27 +0900

    S12P21D104-73 feat: Fix Code

--- yogaback/src/main/java/com/red/yogaback/dto/request/PoseRecordRequest.java
+++ yogaback/src/main/java/com/red/yogaback/dto/request/PoseRecordRequest.java
@@ -5,16 +5,16 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 /**
- * 요가 기록 생성/수정 시 요청 바디
- * - roomRecordId, ranking은 null 가능
+ * 요가 기록 생성/수정 시 JSON으로 전달되는 요청 데이터
+ * roomRecordId, ranking은 null 가능
  */
 @Getter
 @NoArgsConstructor
 @AllArgsConstructor
 public class PoseRecordRequest {
-    private Long roomRecordId;  // null 가능
+    private Long roomRecordId;  // 선택사항 (멀티모드 기록 시)
     private Float accuracy;
-    private Integer ranking;    // null 가능
+    private Integer ranking;    // 선택사항
     private Float poseTime;
-    private String recordImg;
+    // recordImg는 MultipartFile로 별도 받으므로 JSON에는 포함하지 않음
 }
