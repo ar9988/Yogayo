310efb62bdd27f0cbe8246a74a412d950fda0b6a
황선혁 <asd122032@naver.com>
2025-04-05 18:01:36 +0900

    Update YogaYo/app/src/main/java/com/d104/yogaapp/main/MainActivity.kt

--- YogaYo/app/src/main/java/com/d104/yogaapp/main/MainActivity.kt
+++ YogaYo/app/src/main/java/com/d104/yogaapp/main/MainActivity.kt
@@ -108,7 +108,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ){
-                    AppEntry()
+                    MainNavigation()
                 }
             }
         }
