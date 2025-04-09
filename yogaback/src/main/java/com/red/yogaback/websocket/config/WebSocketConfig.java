e0a9bfc9fa7cb0ae6ec398ea4030f90e5c65c837
황홍법 <bup553@naver.com>
2025-04-09 11:08:15 +0900

    feat:Transport 에러 발생시 해당 세션을 꺼내서 DISCONNECT

--- yogaback/src/main/java/com/red/yogaback/websocket/config/WebSocketConfig.java
+++ yogaback/src/main/java/com/red/yogaback/websocket/config/WebSocketConfig.java
@@ -5,20 +5,28 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.ApplicationContextAware;
+import org.springframework.context.ApplicationEventPublisher;
+import org.springframework.context.ApplicationEventPublisherAware;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.Message;
 import org.springframework.messaging.MessageChannel;
 import org.springframework.messaging.simp.SimpMessageType;
 import org.springframework.messaging.simp.config.ChannelRegistration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.messaging.simp.stomp.StompCommand;
 import org.springframework.messaging.simp.stomp.StompHeaderAccessor;
 import org.springframework.messaging.support.ChannelInterceptor;
+import org.springframework.messaging.support.MessageBuilder;
 import org.springframework.messaging.support.MessageHeaderAccessor;
 import org.springframework.scheduling.TaskScheduler;
+import org.springframework.web.socket.CloseStatus;
+import org.springframework.web.socket.WebSocketSession;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
 import org.springframework.web.socket.config.annotation.WebSocketTransportRegistration;
+import org.springframework.web.socket.handler.WebSocketHandlerDecorator;
+import org.springframework.web.socket.messaging.SessionDisconnectEvent;
 
 import com.red.yogaback.websocket.service.UserSessionService;
 import com.red.yogaback.websocket.service.WebSocketConnectionService;
@@ -26,12 +34,16 @@ import com.red.yogaback.websocket.service.UserSession;
 
 @Configuration
 @EnableWebSocketMessageBroker
-public class WebSocketConfig implements WebSocketMessageBrokerConfigurer, ApplicationContextAware {
+public class WebSocketConfig implements WebSocketMessageBrokerConfigurer,
+                                        ApplicationContextAware,
+                                        ApplicationEventPublisherAware {
 
     private final WebSocketAuthChannelInterceptor authChannelInterceptor;
     private final StompLoggingInterceptor stompLoggingInterceptor;
     private final TaskScheduler webSocketTaskScheduler;
+
     private ApplicationContext applicationContext;
+    private ApplicationEventPublisher eventPublisher;
 
     @Autowired
     public WebSocketConfig(
@@ -50,6 +62,11 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer, Applic
                 .setHeartbeatValue(new long[]{30000, 30000})
                 .setTaskScheduler(webSocketTaskScheduler);
         config.setApplicationDestinationPrefixes("/app");
+    }
+
+    @Override
+    public void setApplicationEventPublisher(ApplicationEventPublisher applicationEventPublisher) {
+        this.eventPublisher = applicationEventPublisher;
     }
 
     @Override
@@ -97,11 +114,41 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer, Applic
         registration.interceptors(stompLoggingInterceptor);
     }
 
+    // 5) WebSocket 전송 계층 설정 + TransportError 데코레이터
     @Override
     public void configureWebSocketTransport(WebSocketTransportRegistration registration) {
-        registration.setSendTimeLimit(15 * 1000)
+        registration
+                // I/O 제어: 블로킹 타임아웃, 버퍼 크기, 메시지 크기
+                .setSendTimeLimit(15 * 1000)
                 .setSendBufferSizeLimit(512 * 1024)
-                .setMessageSizeLimit(128 * 1024);
+                .setMessageSizeLimit(128 * 1024)
+
+                // TransportError 발생 시 강제 DISCONNECT 이벤트 발행
+                .addDecoratorFactory(handler -> new WebSocketHandlerDecorator(handler) {
+                    @Override
+                    public void handleTransportError(WebSocketSession session, Throwable exception)
+                            throws Exception {
+                        // 1) 가짜 STOMP DISCONNECT 메시지 생성
+                        StompHeaderAccessor sha = StompHeaderAccessor.create(StompCommand.DISCONNECT);
+                        sha.setSessionId(session.getId());
+                        sha.setSessionAttributes(session.getAttributes());
+                        Message<byte[]> msg = MessageBuilder
+                                .createMessage(new byte[0], sha.getMessageHeaders());
+
+                        // 2) 올바른 생성자로 DISCONNECT 이벤트 발행
+                        eventPublisher.publishEvent(
+                                new SessionDisconnectEvent(
+                                        this,                // source
+                                        msg,                 // fake DISCONNECT message
+                                        session.getId(),     // sessionId
+                                        CloseStatus.SERVER_ERROR
+                                )
+                        );
+
+                        // 3) 기본 핸들러 동작(세션 종료 등) 호출
+                        super.handleTransportError(session, exception);
+                    }
+                });
     }
 
     @Override
